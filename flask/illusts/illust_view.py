4ee848ed8c4c156f2673d81a0c91d71c766b1f92
Zhou Hao <rmusique@gmail.com>
2017-01-07 23:59:31 +0800

    Fix offset/size params BUG in rankings()

--- flask/illusts/illust_view.py
+++ flask/illusts/illust_view.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
 import json
 from mdb import *
 
@@ -33,12 +32,14 @@ def home_page():
 
 # sort_by: total_bookmarks, total_view
 @illust_view.route('/<sort_by>', methods=['GET'], defaults={ 'offset': 0, 'size': 30 })
+@illust_view.route('/<sort_by>/<int:offset>', methods=['GET'], defaults={ 'size': 30 })
+@illust_view.route('/<sort_by>/<int:offset>/<int:size>', methods=['GET'])
 def rankings(sort_by, offset, size):
     illust_helper = IllustHelper()
 
     # get illusts from mongodb
     illusts = []
-    for raw_illust in list(illust_helper.collection.find().sort(sort_by, -1).skip(offset).limit(size)):
+    for raw_illust in list(illust_helper.collection.find().sort(sort_by, 1).skip(offset).limit(size)):
         illust = JsonDict(raw_illust)
         bookmark_rate = float(illust.total_bookmarks) / illust.total_view if illust.total_view > 0 else 0.0
         illust['bookmark_rate'] = bookmark_rate
@@ -46,4 +47,4 @@ def rankings(sort_by, offset, size):
 
     title = 'Illusts排行榜 - %s' % to_utf8(sort_by)
     content = 'SortBy: %s (%d-%d)' % (sort_by, offset+1, offset+size)
-    return render_template('illusts.html', title=title.decode('utf-8'), content=content.decode('utf-8'), illusts=illusts, t=int(time.time()))
+    return render_template('illusts.html', title=title.decode('utf-8'), content=content.decode('utf-8'), illusts=illusts)
